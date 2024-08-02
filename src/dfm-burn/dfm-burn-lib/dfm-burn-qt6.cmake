e7f237afea0e93ddfadbc6674a3bf0fa2b1d1146
Zhang Sheng <zhangsheng@uniontech.com>
2024-08-02 09:19:36 +0800

    refactor: Supports both qt5 and qt6
    If it is a qt6 environment, then two sets of libraries that depend on qt5 and qt6 will be compiled
    
    log: as title

---
+++ src/dfm-burn/dfm-burn-lib/dfm-burn-qt6.cmake
@@ -0,0 +1,6 @@
+set(BIN_NAME dfm6-burn)
+
+set(QT_VERSION_MAJOR 6)
+set(DFM_VERSION_MAJOR 6)
+
+include(dfm-burn.cmake)
---
+++ src/dfm-mount/dfm-mount-qt5.cmake
@@ -0,0 +1,6 @@
+set(BIN_NAME dfm-mount)
+
+set(QT_VERSION_MAJOR 5)
+set(DFM_VERSION_MAJOR "")
+
+include(dfm-mount.cmake)
