02c41d5342e692789ddd7298589171c46cc1a44d
Zhang Sheng <zhangsheng@uniontech.com>
2025-04-03 08:45:02 +0800

    feat: Add dfm-search module with utility header and implementation
    Introduce the dfm-search module, including the addition of searchutility.h and searchutility.cpp files. These files define the SearchUtility namespace and set up the necessary structure for future search-related functionalities.
    
    Log: Establish the foundation for search utilities in the dfm-search module.

---
+++ src/dfm-search/dfm-search-lib/utils/searchutility.h
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2025 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+#ifndef SEARCHUTILITY_H
+#define SEARCHUTILITY_H
+
+#include <dfm-search/dsearch_global.h>
+
+DFM_SEARCH_BEGIN_NS
+
+namespace SearchUtility {
+
+}   // namespace SearchUtility
+
+DFM_SEARCH_END_NS
+
+#endif   // SEARCHUTILITY_H
